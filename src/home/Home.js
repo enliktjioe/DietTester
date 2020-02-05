0ba1da9f080163ffe95c42e61dc1a594500f8c99
Enlik - <enlik@ut.ee>
2020-02-05 22:22:42 +0200

    First design landing page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,42 +1,62 @@
 import * as React from "react";
-import Alert from "react-bootstrap/Alert";
-import {Button} from "react-bootstrap";
-import {useState} from "react";
-
 import { Provider, Heading, Subhead } from 'rebass'
 import {
-  Hero, CallToAction, ScrollDownIndicator
+  Hero, CallToAction, ScrollDownIndicator, Section, Checklist
 } from 'react-landing-page'
 
-function AlertDismissibleExample() {
-    const [show, setShow] = useState(true);
-
-    if (show) {
-        return (
-            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-                <p>
-                    Change this and that and try again. Duis mollis, est non commodo
-                    luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                    Cras mattis consectetur purus sit amet fermentum.
-                </p>
-            </Alert>
-        );
-    }
-    return <Button onClick={() => setShow(true)}>Show Alert</Button>;
-}
+const featherCheckmark = <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24" height="24"
+  viewBox="0 0 24 24"
+  fill="none" stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+>
+  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/>
+  <polyline points="22 4 12 14.01 9 11.01"/>
+</svg>
 
 export class Home extends React.Component {
 
     render() {
-        return (
 
+        return (
             <div id="home-container">
-                {/* <AlertDismissibleExample/> */}
-                <CallToAction href='./logged-in-home'>Get Started</CallToAction>
-                <Button variant="outline-primary" size="lg" href="./logged-in-home" block>
-                    Vajuta siia, et sisse logida
-                </Button>
+                <div>
+                    This is HEADER
+                </div>
+                <div 
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                        }}
+                >
+                    <iframe  src="//www.youtube.com/embed/wRKoRvFwNfY" width="630" height="354"></iframe>
+                </div>
+
+                <div 
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                        }}
+                >
+                    <CallToAction href='./logged-in-home' mt={3}>START YOUR FREE PLAN</CallToAction>
+                </div>
+                
+                <Section
+                    // width={1}
+                    heading="Why you should join?"
+                    subhead="here are some of the science facts"
+                >
+                <Checklist
+                    children={["Benefit 1", "Benefit 2", "Benefit 3"]}
+                    checkmark={featherCheckmark}
+                />
+                </Section>
+
             </div>
         )
     }
