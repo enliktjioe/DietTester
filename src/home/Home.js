4435d2b39e8c5e7cd80fab3011b1eca734133083
Helena <h@asf.com>
2020-02-05 14:45:51 +0200

    react-bootstrap css now applied to components

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,25 +1,33 @@
 import * as React from "react";
 import Alert from "react-bootstrap/Alert";
 import {Button} from "react-bootstrap";
+import {useState} from "react";
+
+function AlertDismissibleExample() {
+    const [show, setShow] = useState(true);
+
+    if (show) {
+        return (
+            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>
+                    Change this and that and try again. Duis mollis, est non commodo
+                    luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+                    Cras mattis consectetur purus sit amet fermentum.
+                </p>
+            </Alert>
+        );
+    }
+    return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+}
 
 export class Home extends React.Component {
 
     render() {
         return (
+
             <div id="home-container">
-                <Alert variant="success">
-                    <Alert.Heading>Hey, nice to see you</Alert.Heading>
-                    <p>
-                        Aww yeah, you successfully read this important alert message. This example
-                        text is going to run a bit longer so that you can see how spacing within an
-                        alert works with this kind of content.
-                    </p>
-                    <hr/>
-                    <p className="mb-0">
-                        Whenever you need to, be sure to use margin utilities to keep things nice
-                        and tidy.
-                    </p>
-                </Alert>
+                <AlertDismissibleExample/>
                 <Button variant="outline-primary" size="lg" href="./logged-in-home" block>
                     Vajuta siia, et sisse logida
                 </Button>
