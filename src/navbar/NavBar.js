54f1154e35792812412a5f524ea763540b6c2089
Ingrid Laurikainen <ingridlaurikainen@gmail.com>
2020-02-05 15:20:02 +0200

    Renewed navbar

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -15,20 +15,15 @@ export class NavBar extends React.Component {
                         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
                         crossOrigin="anonymous"
                     />
-                    <Navbar bg="light" expand="lg">
+                    <Navbar bg="dark" variant="dark" expand="lg">
                         <Navbar.Brand href="#home">Diet Tester</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
                                 <Nav.Link href="./home">Home</Nav.Link>
                                 <Nav.Link href="#link">Food Plans</Nav.Link>
-                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider/>
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
+                            </Nav>
+                            <Nav inline>
                                 <Nav.Link inline href="./logged-in-home">Log In</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
